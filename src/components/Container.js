87b5ec785eef16ddd36fdcc685d4e1df75d6c1a1
Wszymanski987 <85928708+Wszymanski987@users.noreply.github.com>
2022-04-01 16:18:16 +0200

    aktualizacja

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,5 @@
 import react from "react";
-import styled, {css} from "styled-components";
+import styled from "styled-components";
 import LeftBox from "./LeftBox";
 import gym from "../Images/gym.jpg";
 
